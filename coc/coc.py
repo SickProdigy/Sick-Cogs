1df598ba2102b7f6b136327bb2f0b6c8092de18f
sickprodigy <prodigysick@gmail.com>
2024-03-11 20:39:03 -0400

    2 commands -war and -coc, war returns war endpoint -coc returns clan endpoint. You can now set your clan as a guild. Bot owner set's api. So when you type the first two commands it will pull the guild clan instead of default maybe

--- coc/coc.py
+++ coc/coc.py
@@ -1,5 +1,6 @@
 import aiohttp
 import discord
+from datetime import datetime, timedelta
 from redbot.core import Config, commands, checks
 from redbot.core.utils.chat_formatting import box, pagify
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
@@ -13,27 +14,86 @@ class Coc(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        default_global = {"COC_API_KEY": None}
-
+        default_global = {"COC_API_KEY": None, }
+        default_guild = {"COC_CLAN_KEY": None}
         self.config = Config.get_conf(self, 5218831554)
-        self.config.register_guild(**default_global)
-
+        self.config.register_global(**default_global)
+        self.config.register_guild(**default_guild)
+    
+        
+    
     @commands.command()
     async def coc(self, ctx):
-        """Clash of Clan update on if in war or not"""
+        """Clash of Clan information and war results"""
+        """use -cocsetapi and -setcocclankey"""
 
         api_key = await self.config.COC_API_KEY()
         if not api_key:
             return await ctx.send("No API key set for Clash of Clans. Get one at https://developer.clashofclans.com/")
-
+        clan_key = await self.config.COC_CLAN_KEY()
+        if not clan_key:
+            return await ctx.send("No Clan key set for Clash of Clans. Check clan profile, share, copy, paste behind -setcocclankey")
+        clanNameKeyInput = clan_key
+        if '#' in clanNameKeyInput:
+            clanNameKeyInput = clanNameKeyInput.replace('#', "")
+        clanNameConcat = '%23' + clanNameKeyInput
         headers = {
             'Accept': 'application/json',
             'authorization': 'Bearer ' + api_key
         }
+        #return Current Clans endpoint
+        try:
+            async with aiohttp.request('GET', 'https://api.clashofclans.com/v1/clans/' + clanNameConcat, headers=headers) as response:
+                if response.status != 200:
+                    return await ctx.send("Oops! Couldn't return results from COC api...")
+                user_json = await response.json()
+        except aiohttp.ClientConnectionError as e:
+            await ctx.send(f"Oops! Couldn't return results from COC api due to a connection error: {e}")
+        except Exception as e:
+            await ctx.send(f"An unexpected error occurred: {e}")
+        
+        clan_name = str(user_json['name'])
+        clan_tag = user_json['tag']
+        clan_description = user_json['description']
+        members = user_json['members']
+        
+        embed = discord.Embed(
+            description=clan_description,
+            color=0x2ecc71,
+            timestamp=None
+        )
+        image1 = user_json['badgeUrls']['large'] # clan logo thing
+        image2 = 'https://i.imgur.com/TFTXZvP.png' # sg logo
+        image3 = 'https://i.imgur.com/WAZjzZr.jpeg' # coc logo
+        embed.set_author(name=clan_name, icon_url=image1)
+        embed.add_field(name='Join Tag:', value=clan_tag)
+        embed.add_field(name='Member Count:', value=members)
+        
+        embed.set_image(url=image3)
+        embed.set_thumbnail(url=image1)
+        embed.set_footer(text='Brought to you by SickGaming.net', icon_url=image2)
+        
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def war(self, ctx):
+        """Clash of Clan update on if in war or not"""
 
-        clanNameKeyInput = '2QLUUJYVL'
+        api_key = await self.config.COC_API_KEY()
+        if not api_key:
+            return await ctx.send("No API key set for Clash of Clans. Get one at https://developer.clashofclans.com/ and use -setcocapi")
+        
+        clan_key = await self.config.COC_CLAN_KEY()
+        if not clan_key:
+            return await ctx.send("No Clan key set for Clash of Clans. Check clan profile, share, copy, paste. use -setcocclankey")
+        clanNameKeyInput = clan_key
+        if '#' in clanNameKeyInput:
+            clanNameKeyInput = clanNameKeyInput.replace('#', "")
         clanNameConcat = '%23' + clanNameKeyInput
-        truncated_text = ''
+        headers = {
+            'Accept': 'application/json',
+            'authorization': 'Bearer ' + api_key
+        }
         #return Current war endpoint
         try:
             async with aiohttp.request('GET', 'https://api.clashofclans.com/v1/clans/' + clanNameConcat + '/currentwar', headers=headers) as response:
@@ -49,19 +109,80 @@ class Coc(commands.Cog):
         clan_tag = user_json['clan']['tag']
         state_war = user_json['state']
         team_size = user_json['teamSize']
-        embed = discord.Embed(
-            description='Clan tag: ' + clan_tag,
-            color=0x5CDBF0,
-            timestamp=None
-        )
-        image1 = user_json['clan']['badgeUrls']['large']
-        image2 = 'https://i.imgur.com/TFTXZvP.png'
-        embed.set_author(name=clan_name, icon_url=image1)
-        embed.add_field(name ='War State:', value=state_war)
-        embed.add_field(name='Team Size:', value=team_size)
-        embed.set_footer(text='Brought to you by SickGaming.net', icon_url=image2)
-        embed.set_thumbnail(url=image1)
+        team_attacks = user_json['clan']['attacks']
+        team_stars = user_json['clan']['stars']
+        team_destruction = user_json['clan']['destructionPercentage']
+        opponent_attacks = user_json['opponent']['attacks']
+        opponent_stars = user_json['opponent']['stars']
+        opponent_destruction = user_json['opponent']['destructionPercentage']
+        war_prep_time = user_json['preparationStartTime']
+        war_start_time = user_json['startTime']
+        war_end_time = user_json['endTime']
         
+        # Convert time to readable format
+        war_prep_time_converted = datetime.strptime(war_prep_time, "%Y%m%dT%H%M%S.%fZ")
+        war_start_time_converted = datetime.strptime(war_start_time, "%Y%m%dT%H%M%S.%fZ")
+        war_end_time_converted = datetime.strptime(war_end_time, "%Y%m%dT%H%M%S.%fZ")
+        wptc_est = war_prep_time_converted - timedelta(hours=5)
+        wstc_est = war_start_time_converted - timedelta(hours=5)
+        wetc_est = war_end_time_converted - timedelta(hours=5)
+        team_attacks_full_amount = user_json['teamSize']*user_json['attacksPerMember']
+        if state_war == "preparation":
+            embed = discord.Embed(
+                description='Clash of Clan War Status',
+                color=0x2ecc71,
+                timestamp=None
+            )
+            image1 = user_json['clan']['badgeUrls']['large'] # clan logo thing
+            image2 = 'https://i.imgur.com/TFTXZvP.png' # sg logo
+            image3 = 'https://i.imgur.com/WAZjzZr.jpeg' # coc logo
+            embed.set_author(name=clan_name, icon_url=image1)
+            
+            embed.add_field(name ='War State:', value=state_war)
+            embed.add_field(name='Team Size:', value=team_size)
+            embed.add_field(name='Attacks available:', value=team_attacks_full_amount)
+            
+            embed.add_field(name ='War Prep time:', value=wptc_est.strftime("%I:%M %p %b-%d-%Y"))
+            embed.add_field(name ='War Start time:', value=wstc_est.strftime("%I:%M %p %b-%d-%Y"))
+            embed.add_field(name ='War End time:', value=wetc_est.strftime("%I:%M %p %b-%d-%Y"))
+            
+
+            embed.set_image(url=image3)
+            embed.set_thumbnail(url=image1)
+            embed.set_footer(text='Brought to you by SickGaming.net', icon_url=image2)
+
+        elif state_war == "inWar":
+            embed = discord.Embed(
+                description='Clash of Clan War Status. Time is in EST',
+                color=0x992d22,
+                timestamp=None
+            )
+            image1 = user_json['clan']['badgeUrls']['large']
+            image2 = 'https://i.imgur.com/TFTXZvP.png'
+            image3 = 'https://i.imgur.com/WAZjzZr.jpeg' # coc logo
+            embed.set_author(name=clan_name, icon_url=image1)
+            
+            embed.add_field(name ='War State:', value=state_war)
+            embed.add_field(name='Team Size:', value=team_size)
+            embed.add_field(name='Total Attacks:', value=team_attacks_full_amount)
+            
+            embed.add_field(name ='War Prep time:', value=wptc_est.strftime("%I:%M %p %b-%d-%Y"))
+            embed.add_field(name ='War Start time:', value=wstc_est.strftime("%I:%M %p %b-%d-%Y"))
+            embed.add_field(name ='War End time:', value=wetc_est.strftime("%I:%M %p %b-%d-%Y"))
+            
+            # splits them up in 3's automagically, how can I change this?
+            embed.add_field(name ='Attacks Used:', value=team_attacks)
+            embed.add_field(name ='Stars Gained:', value=team_stars)
+            embed.add_field(name ='Team Destruction:', value=team_destruction)
+            
+            embed.add_field(name='Opponent Attacks:', value=opponent_attacks)
+            embed.add_field(name='Opponent Stars:', value=opponent_stars)
+            embed.add_field(name='Opponent Destruction:', value=opponent_destruction)
+            
+
+            embed.set_image(url=image3)
+            embed.set_thumbnail(url=image1)
+            embed.set_footer(text='Brought to you by SickGaming.net', icon_url=image2)
         await ctx.send(embed=embed)
         # await ctx.send (f"image1")
         # await ctx.send(f"'{clan_name}\n{clan_tag}\nState: {state_war}\nTeam Size: {team_size}'") # return results in json format
@@ -71,8 +192,17 @@ class Coc(commands.Cog):
     @checks.is_owner()
     @commands.command(name="setcocapi", aliases=["setcoc"])
     async def _setcocapi(self, ctx, key: str):
-        """Set the api-key for Clash of Clans. Go to clash developer portal for access. Ex: 'Bearer abcdefghijklmnop123456789'"""
+        """Set the api-key for Clash of Clans. Go to clash developer portal for key. Ex: 'abcdefghijklmnop123456789'"""
 
         if key:
             await self.config.COC_API_KEY.set(key)
+            await ctx.send("Key set.")
+
+    @checks.guildowner()
+    @commands.command(name="setcocclankey", aliases=["setcocclan"])
+    async def _setcocclankey(self, ctx, key: str):
+        """Set the Clan Tag for Clash of Clans auto updates."""
+
+        if key:
+            await self.config.COC_CLAN_KEY.set(key)
             await ctx.send("Key set.")
