a615b87a2e7b6240034298fc849c3a4dae13e25d
sickprodigy <prodigysick@gmail.com>
2026-09-06 20:48:42 -0400

    fix(avatar): polish avatar command
    Adds standard cog metadata hooks, accepts Discord users as well as guild members, and cleans up the avatar command response text.

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,27 +1,33 @@
-# Simple avatar URL fetch for Sick-Cogs.
+from typing import Optional, Union
 
-# Discord
 import discord
-
-# Red
 from redbot.core import commands
 
 
 class Avatar(commands.Cog):
     """Get user's avatar URL."""
 
+    __author__ = ["SickProdigy"]
+    __version__ = "1.0.0"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+
     @commands.command()
-    async def avatar(self, ctx, *, user: discord.Member = None):
-        """Returns user avatar URL.
+    async def avatar(
+        self,
+        ctx: commands.Context,
+        *,
+        user: Optional[Union[discord.Member, discord.User]] = None,
+    ):
+        """Return a user's avatar URL.
 
         User argument can be user mention, nickname, username, user ID.
-        Default to yourself when no argument is supplied.
+        Defaults to yourself when no argument is supplied.
         """
-        author = ctx.author
-
-        if not user:
-            user = author
+        user = user or ctx.author
 
         url = user.display_avatar.with_static_format("png")
 
-        await ctx.send(f"{user}'s Avatar URL : {url}")
+        await ctx.send(f"{user}'s avatar URL: {url}")
