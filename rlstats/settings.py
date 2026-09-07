9c09839d2f422125426bd9fed1d8e4c3d0b7d106
SickProdigy <prodigysick@gmail.com>
2026-09-07 13:20:32 +0000

    Update RLStats author attribution

--- rlstats/settings.py
+++ rlstats/settings.py
@@ -1,4 +1,4 @@
-# Copyright 2018-present Jakub Kuczys (https://github.com/Jackenmen)
+# Copyright 2018-present SickProdigy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
