0b02c10853081a299667adffa6845f0e3028074f
SickProdigy <prodigysick@gmail.com>
2026-09-08 14:14:48 +0000

    chore(avatar): align with Red index guidelines
    Refs #87

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,4 +1,8 @@
+from redbot.core.utils import get_end_user_data_statement
 from .avatar import Avatar
+
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
 async def setup(bot):
